7fd013adbe161823f8d2482410bdd94fa1547c7c
= <kudin.play@gmail.com>
2024-09-10 23:10:55 +0300

    release beta 1.0v

--- Main.java
+++ Main.java
@@ -1,28 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        PhoneBook pb = new PhoneBook();
-        Scanner input = new Scanner(System.in);
-        while(true) {
-            System.out.print("Enter action:\n1) Show all contacts\n2) Add new contact\n3) Remove contact\n4) Edit contact\n--> ");
-            switch (input.nextLine()){
-                case "1":
-                    pb.showAllContacts();
-                    break;
-                case "2":
-                    System.out.print("Write the name\n--> ");
-                    String name = input.nextLine();
-                    System.out.print("Write the phone number\n--> ");
-                    String phone = input.nextLine();
-                    pb.addContact(name, phone);
-                    break;
-                case "3":
-                    System.out.print("Enter a name or phone number \n--> ");
-                    String contact = input.nextLine();
-                    pb.removeContact(pb.getId(contact));
-                    break;
-            }
-        }
-    }
+   public static void main(String[] args) {
+	  PhoneBook pb = new PhoneBook();
+//	  pb.addContact("Alex", "001");
+//	  pb.addContact("Bob", "005");
+//	  pb.addContact("Storm", "004");
+//	  pb.addContact("Jackson", "002");
+//	  pb.addContact("Patrik", "003");
+
+	  String contact;
+
+	  Scanner input = new Scanner(System.in);
+	  while(true) {
+		 System.out.print("Enter action:\n1) Show all contacts\n2) Add new contact\n3) Remove contact\n4) Edit contact\n--> ");
+		 switch (input.nextLine()){
+			case "1":
+			   pb.showAllContacts();
+			   break;
+			case "2":
+			   System.out.print("Write the name\n--> ");
+			   String name = input.nextLine();
+			   System.out.print("Write the phone number\n--> ");
+			   String phone = input.nextLine();
+			   System.out.print("Write the email\n--> ");
+			   String email = input.nextLine();
+			   pb.addContact(name, phone, email);
+			   break;
+			case "3":
+			   System.out.print("Enter contact \n--> ");
+			   contact = input.nextLine();
+			   pb.removeContact(pb.getId(contact));
+			   break;
+			case "4":
+			   System.out.print("Enter contact \n--> ");
+			   contact = input.nextLine();
+			   System.out.print("Choose what to change:\n1) Name\n2) Phone\n3) Email\n--> ");
+			   switch (input.nextLine()){
+				  case "1":
+					 System.out.println("Enter a new name\n--> ");
+					 String newName = input.nextLine();
+					 pb.editContactName(pb.getId(contact), newName);
+					 break;
+				  case "2":
+					 System.out.println("Enter a new phone\n--> ");
+					 String newPhone = input.nextLine();
+					 pb.editContactName(pb.getId(contact), newPhone);
+					 break;
+				  case "3":
+					 System.out.println("Enter a new email\n--> ");
+					 String newEmail = input.nextLine();
+					 pb.editContactName(pb.getId(contact), newEmail);
+					 break;
+			   }
+			   break;
+		 }
+	  }
+   }
 }
--- PhoneBook.java
+++ PhoneBook.java
@@ -1,58 +1,74 @@
 import java.util.ArrayList;
 
 public class PhoneBook {
-    ArrayList<String> db_names = new ArrayList<>();
-    ArrayList<String> db_phone = new ArrayList<>();
-    ArrayList<String> db_email = new ArrayList<>();
+   ArrayList<String> db_names = new ArrayList<>();
+   ArrayList<String> db_phone = new ArrayList<>();
+   ArrayList<String> db_email = new ArrayList<>();
 
-    PhoneBook(){}
+   PhoneBook(){}
 
-    // Вивести імена всіх контактів
-    void showAllContacts() {
-        for (String name : db_names) {
-            System.out.println(name);
-        }
-    }
+   // Вивести імена всіх контактів
+   void showAllContacts() {
+	  System.out.println("All contacts:");
+	  for (String name : db_names) {
+		 System.out.println(name);
+	  }
+   }
 
-    // Створення контакта
-    void addContact(String name, String phone) {
-        db_names.add(name);
-        db_phone.add(phone);
-        db_email.add("");
-    }
-    void addContact(String name, String phone, String email) {
-        db_names.add(name);
-        db_phone.add(phone);
-        db_email.add(email);
-    }
+   // Створення контакта
+   void addContact(String name, String phone) {
+	  db_names.add(name);
+	  db_phone.add(phone);
+	  db_email.add("");
+	  System.out.println("Contact " + name + " added");
+   }
+   void addContact(String name, String phone, String email) {
+	  db_names.add(name);
+	  db_phone.add(phone);
+	  db_email.add(email);
+	  System.out.println("Contact " + name + " added");
+   }
 
-    // Знаходження id контакта по імені, або телефону
-    int getId(String contact) {
-        if (db_names.contains(contact)) {
-            return db_names.indexOf(contact);
-        }
-        else if (db_phone.contains(contact)) {
-            return db_phone.indexOf(contact);
-        }
-        else {
-            return -1;
-        }
-    }
+   // Знаходження id контакта по імені, або телефону
+   int getId(String contact) {
+	  // Отримання id по імені
+	  if (db_names.contains(contact)) {
+		 return db_names.indexOf(contact);
+	  }
+	  // Отримання id по номеру телефона
+	  else if (db_phone.contains(contact)) {
+		 return db_phone.indexOf(contact);
+	  }
+	  // Отримання id по адресі елетронної пошти
+	  else if (db_email.contains(contact)) {
+		 return db_email.indexOf(contact);
+	  }
+	  else {
+		 return -1;
+	  }
+   }
 
-    // Видалення контакта
-    void removeContact(int contact_id) {
-        db_names.remove(contact_id);
-        db_phone.remove(contact_id);
-//        db_email.remove(contact_id);
-    }
+   // Видалення контакта
+   void removeContact(int contact_id) {
+	  db_names.remove(contact_id);
+	  db_phone.remove(contact_id);
+	  db_email.remove(contact_id);
+	  System.out.println("Contact " + db_names.get(contact_id) + " deleted");
+   }
 
-    // Редагування імені контакта
-    void editContactName(int contact_id, String new_name) {
-        db_names.set(contact_id, new_name);
-    }
-
-    // Редагування телефона контакта
-    void editContactPhone(int contact_id, String new_phone) {
-        db_phone.set(contact_id, new_phone);
-    }
+   // Редагування імені контакта
+   void editContactName(int contact_id, String new_name) {
+	  db_names.set(contact_id, new_name);
+	  System.out.println("The contact's name has been changed");
+   }
+   // Редагування номера телефона контакта
+   void editContactPhone(int contact_id, String new_phone) {
+	  db_phone.set(contact_id, new_phone);
+	  System.out.println("The contact's phone number has been changed");
+   }
+   // Редагування адреси електронної пошти контакта
+   void editContactEmail(int contact_id, String new_email) {
+	  db_phone.set(contact_id, new_email);
+	  System.out.println("The contact's email has been changed");
+   }
 }
